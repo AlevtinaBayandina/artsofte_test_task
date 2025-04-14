bf0c138221493fbf8e17dac7aa3defbfdc734466
Bayandina <bayandina_am@sima-land.ru>
2025-04-14 15:26:58 +0500

    creating base intities

--- src/Entity/Car.php
+++ src/Entity/Car.php
@@ -3,20 +3,82 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
-use Symfony\Component\Serializer\Attribute\Groups;
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
 
-#[ApiResource(
-        operations: [
-            new Get(normalizationContext: ['groups' => 'car:item']),
-            new GetCollection(normalizationContext: ['groups' => 'car:list'])
-        ],
-    paginationEnabled: false,
-)]
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+#[ApiResource]
 class Car
 {
-    #[Groups(['car:list', 'car:item'])]
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $price = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cars')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Brand $brandID = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cars')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Model $modelID = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getBrandID(): ?Brand
+    {
+        return $this->brandID;
+    }
+
+    public function setBrandID(?Brand $brandID): static
+    {
+        $this->brandID = $brandID;
+
+        return $this;
+    }
+
+    public function getModelID(): ?Model
+    {
+        return $this->modelID;
+    }
+
+    public function setModelID(?Model $modelID): static
+    {
+        $this->modelID = $modelID;
+
+        return $this;
+    }
 }
---
+++ src/Entity/Model.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ModelRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ModelRepository::class)]
+#[ApiResource]
+class Model
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Car>
+     */
+    #[ORM\OneToMany(targetEntity: Car::class, mappedBy: 'modelID')]
+    private Collection $cars;
+
+    public function __construct()
+    {
+        $this->cars = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Car>
+     */
+    public function getCars(): Collection
+    {
+        return $this->cars;
+    }
+
+    public function addCar(Car $car): static
+    {
+        if (!$this->cars->contains($car)) {
+            $this->cars->add($car);
+            $car->setModelID($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCar(Car $car): static
+    {
+        if ($this->cars->removeElement($car)) {
+            // set the owning side to null (unless already changed)
+            if ($car->getModelID() === $this) {
+                $car->setModelID(null);
+            }
+        }
+
+        return $this;
+    }
+}
